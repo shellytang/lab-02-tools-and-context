e1603a5ec3a7e5eb2a57080dea1df7868eece6f3
Shelly Tang <shellytang@gmail.com>
2017-04-19 10:16:53 -0700

    dev dependency for eslint

--- lab-shelly/lib/callApply.js
+++ lab-shelly/lib/callApply.js
@@ -5,26 +5,26 @@ module.exports = exports = {};
 exports.Pet = function(name, type) {
   this.name = name;
   this.type = type;
-  return `${name} is a ${type}`
+  return `${name} is a ${type}`;
 };
 //call example
 exports.Cat = function(name, type) {
   exports.Pet.call(this, name, type);
   this.isFluffy = true;
   this.treat = 'catnip';
-  return `${name} is a ${type} who likes ${this.treat}`
+  return `${name} is a ${type} who likes ${this.treat}`;
 };
 //apply example
 exports.Fish = function(name, type) {
   exports.Pet.apply(this, [name, type]);
   this.isFluffly = false;
   this.treat = 'worms';
-  return `${name} is a ${type} who likes ${this.treat}`
-}
+  return `${name} is a ${type} who likes ${this.treat}`;
+};
 //bind example - used example similar to MPN documentation(creating bound function)
 exports.greeting = {
   greet: 'meow',
   sayHi: function() {
     return this.greet;
-  }
-}
+  },
+};
